78e06d2f7d0af88379f43cfd1ac922ad0c88493c
gleva <gleva@192.168.0.25>
2021-09-06 21:44:30 +0200

    handle error

--- form/src/main/resources/js/element/submitForm.js
+++ form/src/main/resources/js/element/submitForm.js
@@ -5,7 +5,10 @@ $.fn.submitForm = function (options) {
 	
 	return this.each(function() {
 		let form = $(this).closestForm();
-		options.data =  $(form).objectifyForm();
+		options.data =  form.objectifyForm();
+		options.error = (errorMessage) => {
+			form.handleError(errorMessage);
+		};
 		$.postJson(options);
 	});
 }
